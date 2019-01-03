98cf6d9901a8ce2f787a7862e58967b45297c695
CogDog <cogdogblog@gmail.com>
2019-01-03 00:07:48 -0600

    Committed We Are

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# font-awesome-5-menus
-WordPress plugin for adding Font Awesome Icons to Menus
+# Font Awesome 5 For Menus
+by Alan Levine http://cogdog.info/ or http://cogdogblog.com/
+
+-----
+*If this kind of stuff has any value to you, please consider supporting me so I can do more!*
+
+[![Support me on Patreon](http://cogdog.github.io/images/badge-patreon.png)](https://patreon.com/cogdog) [![Support me on via PayPal](http://cogdog.github.io/images/badge-paypal.png)](https://paypal.me/cogdog)
+
+----- 
+
+A slight tweak to the [Font Awesome 4 Menus plugin](https://wordpress.org/plugins/font-awesome-4-menus/) updated work with Font Awesome 5.
+
+
+
+This is a first cut test to see if the thing even works... documentation just may follow. Below are some mods of the original plugin readme.
+
+## Description
+
+Add Font Awesome 5 icons to your WordPress menus and site without touching a single line of code! With this plugin, just add the full Font Awesome classes, e.g. `fab fa-(icon name)`  as a class/classes to your menu and the plugin will pull that out, put the icon before or after your link text, and wrap your link text in a span so you can show or hide it as you see fit.
+
+In Font Awesome 5 Menus, we have added the ability to add multiple Font Awesome classes for multiple effects (eg, fa-home fa-fw fa-2x will add a fixed-width home icon at two times the normal size).
+
+In addition, we have given you shortcodes to add icons to your posts and pages, as well as shortcodes to take advantage of the new stacked feature of Font Awesome 5 (shortcodes have yet to be tested)
+
+## Installation
+
+Download this repository as a .zip file then upload it as a plugin in your WordPress dashboard. 
+
+Then, activate the plugin through the 'Plugins' menu in WordPress. Look for a menu item under the **Settings** menu.
+
+## Frequently Asked Questions
+
+### Where can I find all of the available icons? =
+
+Head over to [the Font Awesome website](https://fontawesome.com/icons "the Font Awesome website") to find a full list of icons available. The plugin works with all free icons.
+
+= How to I add an icon to my menu? =
+
+Go to **Appearance** -> **Menus**, select which menu item to which you want to add the icon, and add the icon class(es) under 'CSS Classes (optional)'. (eg, to add the [home icon](https://fontawesome.com/icons/home?style=solid) to your 'Home' link, enter "fas fa-home" (without quotes) as a class. To make it spin, add "fas fa-home fa-spin" as your classes.) Save your menu and voila!
+
+## Updates
+
+5.0 - the very first attempt to get this to work, just tweaked the original plugin slightly.
+.
+
+
+
+
+
---
+++ n9m-font-awesome-5.php
@@ -0,0 +1,235 @@
+<?php
+/*
+Plugin Name: Font Awesome 5 Menus
+Plugin URI: https://github.com/cogdog/font-awesome-5-menus
+Description: Easily add Font Awesome 5.0 icons to your WordPress menus and anywhere else on your site! This is a tweak to original version 4.7.0 plugin by New Nine Media
+Version: 5.0
+Author: CogDog
+Author URI: https://cog.dog/
+License: GPLv2 or later
+*/
+
+/*
+    Copyright 2013-2016  NEW NINE MEDIA  (tel : +1-800-288-9699)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as 
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+class FontAwesomeFive {
+
+    public static $defaults = array(
+        'fa5_location' => 'https://use.fontawesome.com/releases/v5.6.3/css/all.css',
+        'spacing' => 1,
+        'stylesheet' => 'local',
+        'version' => '5.0'
+    );
+
+    function __construct(){
+        global $wp_version;
+
+        add_action( 'admin_enqueue_scripts', array( $this, 'admin_enqueue_scripts' ) );
+        add_action( 'admin_menu', array( $this, 'admin_menu' ) );
+        add_action( 'wp_enqueue_scripts', array( $this, 'wp_enqueue_scripts' ) );
+
+        add_filter( 'nav_menu_css_class', array( $this, 'nav_menu_css_class' ) );
+        add_filter( 'walker_nav_menu_start_el', array( $this, 'walker_nav_menu_start_el' ), 10, 4 );
+
+        add_shortcode( 'fa', array( $this, 'shortcode_icon' ) );
+        add_shortcode( 'fa-stack', array( $this, 'shortcode_stack' ) );
+    }
+
+    function admin_enqueue_scripts( $hook ){
+        if( 'settings_page_n9m-font-awesome-5-menus' == $hook ){
+            wp_enqueue_style( 'n9m-admin-font-awesome-5', plugins_url( 'css/all.min.css', __FILE__ ), false, self::$defaults[ 'version' ] );
+        }
+    }
+
+    function admin_menu(){
+        add_submenu_page( 'options-general.php', 'Font Awesome 5 Menus', 'Font Awesome', 'edit_theme_options', 'n9m-font-awesome-5-menus', array( $this, 'admin_menu_cb' ) );
+    }
+
+    function admin_menu_cb(){
+        if( $_POST && check_admin_referer( 'n9m-fa' ) ){
+            $settings = array();
+            switch( $_POST[ 'n9m_location' ] ){
+                case 'local':
+                case 'fa5':
+                case 'none':
+                    $settings[ 'stylesheet' ] = $_POST[ 'n9m_location' ];
+                    break;
+                case 'other':
+                    $settings[ 'stylesheet' ] = 'other';
+                    $settings[ 'stylesheet_location' ] = sanitize_text_field( $_POST[ 'n9m_location-other-location' ] );
+                    break;
+            }
+            if( isset( $_POST[ 'n9m_text_spacing' ] ) ){
+                $settings[ 'spacing' ] = 1;
+            } else {
+                $settings[ 'spacing' ] = 0;
+            }
+            update_option( 'n9m-font-awesome-5-menus', $settings );
+            print '<div class="updated"><p>Your settings have been saved!</p></div>';
+        }
+        $settings = get_option( 'n9m-font-awesome-5-menus', self::$defaults );
+        print ' <div class="wrap">
+                    <h2><i class="fas fa-dog"></i> ' . get_admin_page_title() . ' by @cogdog</h2>
+                    <form action="' . admin_url( 'options-general.php?page=n9m-font-awesome-5-menus' ) . '" method="post">
+                        <h3>Font Awesome Stylesheet</h3>
+                        <p>Select how you want Font Awesome 5&#8217;s stylesheet loaded on your site (if at all):</p>
+                        <table class="form-table">
+                            <tbody>
+                                <tr>
+                                    <th scope="row">Load Font Awesome 5 From:</th>
+                                    <td>
+                                        <fieldset>
+                                            <legend class="screen-reader-text"><span>Load Font Awesome 5 From</span></legend>
+                                            <label for="n9m_location-local"><input type="radio" name="n9m_location" id="n9m_location-local" value="local"' . ( 'local' == $settings[ 'stylesheet' ] ? ' checked' : false ) . '> Local plugin folder (default)</label>
+                                            <br />
+                                            <label for="n9m_location-fa5"><input type="radio" name="n9m_location" id="n9m_location-fa5" value="fa5"' . ( 'fa5' == $settings[ 'stylesheet' ] ? ' checked' : false ) . '> Official Font Awesome CDN <span class="description">(<a href="https://fontawesome.com" target="_blank">Font Awesome Itself</a>)</span></label>
+                                            <br />
+                                            <label for="n9m_location-other"><input type="radio" name="n9m_location" id="n9m_location-other" value="other"' . ( 'other' == $settings[ 'stylesheet' ] ? ' checked' : false ) . '> A custom location:</label> <input type="text" name="n9m_location-other-location" id="n9m_location-other-location" placeholder="Enter full url here" class="regular-text" value="' . ( isset( $settings[ 'stylesheet_location' ] ) ? $settings[ 'stylesheet_location' ] : '' ) . '">
+                                            <br />
+                                            <label for="n9m_location-none"><input type="radio" name="n9m_location" id="n9m_location-none" value="none"' . ( 'none' == $settings[ 'stylesheet' ] ? ' checked' : false ) . '> Don&#8217;t load Font Awesome 5&#8217;s stylesheet <span class="description">(use this if you load Font Awesome 5 elsewhere on your site)</span></label>
+                                        </fieldset>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <h3>Icon Spacing</h3>
+                        <p>By default, Font Awesome 5 Menus adds a space before or after the icon in your menu. Uncheck the box below to remove this space and give you finer control over your custom styling.</p>
+                        <p><label for="n9m_text_spacing"><input type="checkbox" name="n9m_text_spacing" id="n9m_text_spacing" value="1"' . ( 1 == $settings[ 'spacing' ] ? ' checked' : false ) . '> Keep the space between my text and my icons <span class="description">(default is checked)</span></label>
+                        <p>' . wp_nonce_field( 'n9m-fa' ) . '<button type="submit" class="button button-primary">Save Settings</button></p>
+                    </form>
+                </div>';
+    }
+
+    function nav_menu_css_class( $classes ){
+        if( is_array( $classes ) ){
+            $tmp_classes = preg_grep( '/^(fa)(-\S+)?$/i', $classes );
+            if( !empty( $tmp_classes ) ){
+                $classes = array_values( array_diff( $classes, $tmp_classes ) );
+            }
+        }
+        return $classes;
+    }
+
+    public static function register_uninstall_hook(){
+        if( current_user_can( 'delete_plugins' ) ){
+            delete_option( 'n9m-font-awesome-5-menus' );
+            $users_with_meta = get_users( array(
+                'meta_key' => 'n9m-font-awesome-5-notice-hide',
+                'meta_value' => 1
+            ) );
+            foreach( $users_with_meta as $user_with_meta ){
+                delete_user_meta( $user_with_meta->ID, 'n9m-font-awesome-5-notice-hide' );
+            }
+        }
+    }
+
+    protected function replace_item( $item_output, $classes ){
+        $settings = get_option( 'n9m-font-awesome-5-menus', FontAwesomeFive::$defaults );
+        $spacer = 1 == $settings[ 'spacing' ] ? ' ' : '';
+
+        $before = true;
+        if( in_array( 'fa-after', $classes ) ){
+            $classes = array_values( array_diff( $classes, array( 'fa-after' ) ) );
+            $before = false;
+        }
+
+        $icon = '<span class="' . implode( ' ', $classes ) . '"></span>';
+
+        preg_match( '/(<a.+>)(.+)(<\/a>)/i', $item_output, $matches );
+        if( 4 === count( $matches ) ){
+            $item_output = $matches[1];
+            if( $before ){
+                $item_output .= $icon . '<span class="fontawesome-text">' . $spacer . $matches[2] . '</span>';
+            } else {
+                $item_output .= '<span class="fontawesome-text">' . $matches[2] . $spacer . '</span>' . $icon;
+            }
+            $item_output .= $matches[3];
+        }
+        return $item_output;
+    }
+    
+    function shortcode_icon( $atts ){
+        $a = shortcode_atts( array(
+            'class' => ''
+        ), $atts );
+        if( !empty( $a[ 'class' ] ) ){
+            $class_array = explode( ' ', $a[ 'class' ] );
+            if( !in_array( 'fa', $class_array ) ){
+                $class_array[] = 'fa';
+            }
+            return '<i class="' . implode( ' ', $class_array ) . '"></i>';
+        }
+    }
+    
+    function shortcode_stack( $atts, $content = null ){
+        $a = shortcode_atts( array(
+            'class' => ''
+        ), $atts );
+        $class_array = array();
+        if( empty( $a[ 'class' ] ) ){
+            $class_array = array( 'fa-stack' );
+        } else {
+            $class_array = explode( ' ', $a[ 'class' ] );
+            if( !in_array( 'fa-stack', $class_array ) ){
+                $class_array[] = 'fa-stack';
+            }
+        }
+        return '<span class="' . implode( ' ', $class_array ) . '">' . do_shortcode( $content ) . '</span>';
+    }
+
+    function walker_nav_menu_start_el( $item_output, $item, $depth, $args ){
+        if( is_array( $item->classes ) ){
+            $classes = preg_grep( '/^(fa)(-\S+)?$/i', $item->classes );
+            if( !empty( $classes ) ){
+                $item_output = $this->replace_item( $item_output, $classes );
+            }
+        }
+        return $item_output;
+    }
+
+    function wp_enqueue_scripts(){
+        $settings = get_option( 'n9m-font-awesome-5-menus', self::$defaults );
+        switch( $settings[ 'stylesheet' ] ){
+            case 'local':
+                wp_register_style( 'font-awesome-five', plugins_url( 'css/all.min.css', __FILE__ ), array(), self::$defaults[ 'version' ], 'all' );
+                wp_enqueue_style( 'font-awesome-five' );
+                break;
+            case 'fa5':
+                wp_register_style( 'font-awesome-five', self::$defaults[ 'fa5_location' ], array(), self::$defaults[ 'version' ], 'all' );
+                wp_enqueue_style( 'font-awesome-five' );
+                break;
+            case 'none':
+                break;
+            case 'other':
+                wp_register_style( 'font-awesome-five', $settings[ 'stylesheet_location' ], array(), self::$defaults[ 'version' ], 'all' );
+                wp_enqueue_style( 'font-awesome-five' );
+                break;
+        }
+    }
+
+    public static function write_log( $log ){
+        if( is_array( $log ) || is_object( $log ) ){
+            error_log( print_r( $log, true ) );
+        } else {
+            error_log( $log );
+        }
+    }
+    
+}
+$n9m_font_awesome_five = new FontAwesomeFive();
+
+register_uninstall_hook( __FILE__, array( 'FontAwesomeFive', 'register_uninstall_hook' ) );
